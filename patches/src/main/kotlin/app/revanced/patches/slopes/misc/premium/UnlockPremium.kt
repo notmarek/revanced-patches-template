eef2d3af2dadb5925700cd4fb5c7b389272f54c2
Marek Veselý <notmarek1337@gmail.com>
2025-01-27 21:15:13 +0100

    slopes

---
+++ patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/UnlockPremium.kt
@@ -0,0 +1,36 @@
+package app.revanced.patches.slopes.misc.premium
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.bytecodePatch
+
+@Suppress("unused")
+val unlockPremiumPatch = bytecodePatch(
+    name = "Unlock Premium",
+) {
+    compatibleWith("com.consumedbycode.slopes")
+
+    execute {
+        getCurrentSubscriptionType.method.addInstructions(
+            0,
+            """
+                sget-object v0, Lcom/consumedbycode/slopes/vo/SubscriptionOrigin;->OTHER:Lcom/consumedbycode/slopes/vo/SubscriptionOrigin;
+
+                return-object v0
+            """,
+        )
+
+        getPassExpiration.method.addInstructions(
+            0,
+            """
+                const-wide/32 v1, 0xbac9bd7cL
+
+                invoke-static {v1, v2}, Ljava/time/Instant;->ofEpochSecond(J)Ljava/time/Instant;
+                
+                move-result-object v0
+
+                return-object v0
+            """,
+        )
+
+    }
+}
