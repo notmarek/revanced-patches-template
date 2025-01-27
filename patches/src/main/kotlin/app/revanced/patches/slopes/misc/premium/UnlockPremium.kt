e598fcc7c08423b4dabd8a52e9e8c53b6ef7259c
Marek Veselý <notmarek1337@gmail.com>
2025-01-27 21:25:41 +0100

    ksdajidsj

--- patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/UnlockPremium.kt
+++ patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/UnlockPremium.kt
@@ -2,24 +2,6 @@ package app.revanced.patches.slopes.misc.premium
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.bytecodePatch
-import app.revanced.patcher.fingerprint
-import com.android.tools.smali.dexlib2.AccessFlags
-
-internal val getCurrentSubscriptionType = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
-    returns("Lcom/consumedbycode/slopes/vo/SubscriptionOrigin")
-    custom { method, classDef ->
-        classDef.endsWith("AccessController;") && method.name == "getCurrentSubscriptionType"
-    }
-}
-
-internal val getPassExpiration = fingerprint {
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
-    returns("Ljava/time/Instant")
-    custom { method, classDef ->
-        classDef.endsWith("AccessController;") && method.name == "getPassExpiration"
-    }
-}
 
 @Suppress("unused")
 val unlockPremiumPatch = bytecodePatch(
