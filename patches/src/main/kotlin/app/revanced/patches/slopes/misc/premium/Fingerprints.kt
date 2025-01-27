c8971806199ae50825e8a85f0313523216301d76
Marek Veselý <notmarek1337@gmail.com>
2025-01-27 21:34:52 +0100

    idk ;(((

--- patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/Fingerprints.kt
+++ patches/src/main/kotlin/app/revanced/patches/slopes/misc/premium/Fingerprints.kt
@@ -1,2 +1,20 @@
 package app.revanced.patches.slopes.misc.premium
 
+import app.revanced.patcher.fingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+
+internal val getCurrentSubscriptionType = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("Lcom/consumedbycode/slopes/vo/SubscriptionOrigin")
+    custom { method, classDef ->
+        classDef.endsWith("AccessController;") && method.name == "getCurrentSubscriptionType"
+    }
+}
+
+internal val getPassExpiration = fingerprint {
+    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+    returns("Ljava/time/Instant")
+    custom { method, classDef ->
+        classDef.endsWith("AccessController;") && method.name == "getPassExpiration"
+    }
+}
